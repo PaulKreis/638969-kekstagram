dfe8edc0dfcb2235aea714f9aaf0ad8d858981c7
Pavel Kreys <kreislab@gmail.com>
2018-06-15 22:10:25 +0300

    Правки по комментариям №0

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,6 @@
 'use strict';
-var jsObjects = [];
+
+var photos = [];
 
 var commentsData = [
   'Всё отлично!',
@@ -9,6 +10,7 @@ var commentsData = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
+
 var descriptionData = [
   'Тестим новую камеру!',
   'Затусили с друзьями на море',
@@ -17,60 +19,71 @@ var descriptionData = [
   'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
   'Вот это тачка!'
 ];
-var getRandom = function (min, max) {
+
+var LIKES_MIN = 15;
+var LIKES_MAX = 200;
+var NUMBER_OF_COMMENTS = 2;
+var NUMBER_OF_PHOTO = 25;
+var MAX_PHRASE_IN_COMMENTS = 2;
+
+var getRandomNum = function (min, max) {
   return Math.round(Math.random() * (max - min) + min);
 };
-var generateJsObject = function (urlNum) {
-  var jsObject = {};
+
+//  Создаем массив из 25 сгенерированных JS объектов, которые будут описывать фотографии
+var generatePhoto = function (urlNum) {
+  var photo = {};
   var commentsArray = [];
-  jsObject.url = 'photos/' + urlNum + '.jpg';
-  jsObject.likes = getRandom(15, 200);
-  for (var i = 0; i < 2; i++) {
+  photo.url = 'photos/' + urlNum + '.jpg';
+  photo.likes = getRandomNum(LIKES_MIN, LIKES_MAX);
+  for (var i = 0; i < NUMBER_OF_COMMENTS; i++) {
     commentsArray[i] = '';
-    for (var j = 0; j < getRandom(1, 2); j++) {
-      commentsArray[i] += commentsData[getRandom(0, 5)];
+    for (var j = 0; j < getRandomNum(1, MAX_PHRASE_IN_COMMENTS); j++) {
+      commentsArray[i] += commentsData[getRandomNum(0, commentsData.length - 1)];
     }
   }
-  jsObject.comments = commentsArray;
-  jsObject.description = descriptionData[getRandom(0, 5)];
-  return jsObject;
+  photo.comments = commentsArray;
+  photo.description = descriptionData[getRandomNum(0, descriptionData.length - 1)];
+  return photo;
 };
 
-for (var i = 0; i < 25; i++) {
-  jsObjects[i] = generateJsObject(i + 1);
+for (var i = 0; i < NUMBER_OF_PHOTO; i++) {
+  photos[i] = generatePhoto(i + 1);
 }
 
+//  Показываем сетку из фотографий
 var pictureTemplate = document.querySelector('#picture').content;
 var pictureGrid = document.querySelector('.pictures');
 var fragment = document.createDocumentFragment();
 
-for (i = 0; i < 25; i++) {
+for (i = 0; i < NUMBER_OF_PHOTO; i++) {
   var pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = jsObjects[i].url;
-  pictureElement.querySelector('.picture__stat--likes').textContent = jsObjects[i].likes;
-  pictureElement.querySelector('.picture__stat--comments').textContent = jsObjects[i].comments;
+  pictureElement.querySelector('.picture__img').src = photos[i].url;
+  pictureElement.querySelector('.picture__stat--likes').textContent = photos[i].likes;
+  pictureElement.querySelector('.picture__stat--comments').textContent = photos[i].comments;
   fragment.appendChild(pictureElement);
 }
 
 pictureGrid.appendChild(fragment);
 
+//  Показываем и заполняем элемент .big-picture данными из первого элемента массива
 var bigPic = document.querySelector('.big-picture');
 bigPic.classList.remove('hidden');
 
-bigPic.querySelector('.big-picture__img').src = jsObjects[0].url;
-bigPic.querySelector('.likes-count').textContent = jsObjects[0].likes;
+bigPic.querySelector('.big-picture__img').src = photos[0].url;
+bigPic.querySelector('.likes-count').textContent = photos[0].likes;
 
-bigPic.querySelector('.comments-count').textContent = jsObjects[0].comments.length;
-bigPic.querySelector('.social__caption').textContent = jsObjects[0].description;
+bigPic.querySelector('.comments-count').textContent = photos[0].comments.length;
+bigPic.querySelector('.social__caption').textContent = photos[0].description;
 
 var comments = document.querySelectorAll('.social__comment');
 
-
 for (i = 0; i < comments.length; i++) {
-  comments[i].querySelector('.social__picture').src = 'img/avatar-' + getRandom(1, 6) + '.svg';
-  comments[i].querySelector('.social__text').textContent = jsObjects[i].comments[i];
+  comments[i].querySelector('.social__picture').src = 'img/avatar-' + getRandomNum(1, 6) + '.svg';
+  comments[i].querySelector('.social__text').textContent = photos[i].comments[i];
 }
 
+//  Прячем блоки счётчика комментариев и загрузки новых комментариев
 var sCount = document.querySelector('.social__comment-count');
 sCount.classList.add('visually-hidden');
 var sCountLoadMore = document.querySelector('.social__loadmore');
