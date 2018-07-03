138317473d1efa78201f103d12131e486d9e692e
Pavel Kreis <kreislab@gmail.com>
2018-07-03 13:19:29 +0300

    Оптимизация кода

--- js/gallery.js
+++ js/gallery.js
@@ -5,10 +5,23 @@
       var picturesFragment = document.createDocumentFragment();
 
       photos.forEach(function (photo) {
-        window.picture.render(picturesFragment, photo);
+        renderPicture(picturesFragment, photo);
       });
 
       target.appendChild(picturesFragment);
     }
+  };
+
+  var pictureTemplate = document.querySelector('#picture').content;
+  var renderPicture = function (picturesFragment, photo) {
+    var pictureElement = pictureTemplate.cloneNode(true);
+    var pictureImg = pictureElement.querySelector('.picture__img');
+    pictureElement.querySelector('.picture__img').src = photo.url;
+    pictureElement.querySelector('.picture__stat--likes').textContent = photo.likes;
+    pictureElement.querySelector('.picture__stat--comments').textContent = photo.comments.length;
+    picturesFragment.appendChild(pictureElement);
+    pictureImg.addEventListener('click', function () {
+      window.preview.render(photo);
+    });
   };
 })();
--- js/utils.js
+++ js/utils.js
@@ -12,7 +12,7 @@
     showErrorMsg: function (errorMsgTxt) {
       var errorMsg = document.querySelector('.img-upload__message--error');
       errorMsg.classList.remove('hidden');
-      errorMsg.style.zIndex = 3;
+      errorMsg.style.zIndex = 2;
 
       errorMsg.innerHTML = errorMsgTxt + '<div class="error__links"> <a class="error__link" href="index.html">Попробовать снова</a> <a class="error__link" href="index.html">Загрузить другой файл</a> </div>';
     }
